21d1966ff4ebc316304249a5086be0131c859588
GHANIME <ghanime.charles@gmail.com>
2018-11-20 11:41:56 +0100

    debbugage strlcat OK

--- Libft/MAINS/ft_strlcat.c
+++ Libft/MAINS/ft_strlcat.c
@@ -6,11 +6,12 @@
 /*   By: cghanime <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/17 17:29:04 by cghanime          #+#    #+#             */
-/*   Updated: 2018/11/20 11:41:13 by cghanime         ###   ########.fr       */
+/*   Updated: 2018/11/20 11:40:00 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
@@ -38,4 +39,32 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dst[dst_len] = '\0';
 	return (src_len);
 }
+	/*dst_len = ft_strlen(dst);
+	src_len = ft_strlen(src);
+	if (dst_len + src_len  >= total_len)
+		return (dst_len + src_len);
+	ft_memcpy(dst + dst_len, src, src_len + 1);
+	return (dst_len + src_len);
+}*/
+		
+	/*while (dti ='0)	+;
+	j = 0;
+	while (j <= srcsize)
+	{
+		dst[i] = src[j];
+		i++;
+		j++;
+	}
+	dst[i] = '\0';
+	return (dstsize + srcsize + 1);
+}*/
 
+int main(void)
+{
+	char dst[100] = "saluttotototo";
+	char dst_1[100] = "saluttotototo";
+
+	printf("%zu |%s|\n", strlcat(dst, "coucou", 7), dst);
+	printf("%zu |%s|\n", ft_strlcat(dst_1, "coucou", 7), dst_1);
+	return (0);
+}
