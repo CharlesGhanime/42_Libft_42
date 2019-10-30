8c35c726b0226e5476bb2d7a4d9e055866f4889f
Charles <ghanime.charles@gmail.com>
2019-10-30 20:24:54 +0100

    libft a jour

--- ft_ptrnew.c
+++ ft_ptrnew.c
@@ -6,18 +6,18 @@
 /*   By: cghanime <cghanime@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/10 19:28:24 by cghanime          #+#    #+#             */
-/*   Updated: 2019/10/10 20:02:05 by cghanime         ###   ########.fr       */
+/*   Updated: 2019/10/30 14:47:40 by cghanime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_ptrnew(size_t size)
+void	*ft_ptrnew(size_t len, size_t size)
 {
 	void		*ptr;
 
-	if (!(ptr = malloc(sizeof(void *) * size)))
+	if (!(ptr = malloc(size * len)))
 		return (NULL);
-	ft_bzero(ptr, size);
+	ft_bzero(ptr, size * len);
 	return (ptr);
 }
